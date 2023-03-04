3a84a00331dfdd16639f950bb70cffe95280c49d
Nilesh <mnilesh50@gmail.com>
2023-03-04 12:53:09 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InversioAndDependency
+{
+    class Program
+    {
+        public enum logenum { FileLogger, DBLogger};
+    
+        static void Main(string[] args)
+        {
+            logenum value = logenum.FileLogger ;
+            ILogger log=null;
+
+            switch (value)
+            {
+                case logenum.DBLogger:
+                    log = new DataBaseLogger();
+                    break;
+                case logenum.FileLogger:
+                    log = new FileLogger();
+                    break;              
+             }
+
+            ProductServices prod = new ProductServices(log);
+            prod.Log("Inversion Control");
+        }
+    }
+
+    class ProductServices : ILogger
+    {
+        private readonly  ILogger loggerObject;
+
+        public ProductServices(ILogger logger)
+        {
+            this.loggerObject = logger;
+        }
+        public void Log(string message)
+        {
+            loggerObject.Log(message);
+        }
+    }
+
+    class FileLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine("This is File Logger :"+ message);
+        }
+    }
+    class DataBaseLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine("This is Database Logger :"+ message);
+        }
+    }
+
+    interface ILogger
+    {
+        void Log(string messae);
+    }
+}
